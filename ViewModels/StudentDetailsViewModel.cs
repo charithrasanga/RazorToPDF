815b871146d439cf046e2584307d058cbc8ff8f5
Charith Wikramasinghe <charith.w@azendtech.com>
2024-05-06 11:58:38 +0530

    implimented the service and merged with main

---
+++ ViewModels/StudentDetailsViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace RazorToPDF.ViewModels
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        NonBinary,
+        PreferNotToShare
+    }
+
+    public enum AEDiscountType
+    {
+        Percentage,
+        Amount
+    }
+
+    public class StudentMockDataViewModel
+    {
+        public IntialIntakeMockData IntialIntakeData { get; set; }
+    }
+
+    public class IntialIntakeMockData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public Gender Gender { get; set; }
+        public string IntakeBy { get; set; }
+        public string Service { get; set; }
+        public string CenterAEFee { get; set; }
+        public string AEDiscount { get; set; }
+        public AEDiscountType AEDiscountType { get; set; }
+        public double AEDiscountAmount { get; set; }
+        public double DiscounttedAEFee { get; set; }
+        public string Promocode { get; set; }
+        public string IntakeNote { get; set; }
+    }
+}
